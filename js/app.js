d10df4ac9d991ed8a4ca364a6cb3bf51711f0a1c
zacboz <zacboz@gmail.com>
2017-03-29 22:47:23 -0600

    nav

--- js/app.js
+++ js/app.js
@@ -1,7 +1,38 @@
 angular.module('makeTracks', ['ui.router'])
   .config(function($stateProvide, $urlRouterProvider){
 
+    $stateProvider
+                .state('home',{
+                    url:'/',
+                    templateUrl: "./views/home.html"
+                })
+                .state('story',{
+                    url:'/story',
+                    templateUrl: './views/story.html'
+                })
+                .state('hammocks',{
+                    url:'/hammocks',
+                    templateUrl: './views/hammocks.html',
+                    controller: 'hammocksCtrl'
+                })
+                .state('outpost',{
+                    url:'/outpost',
+                    templateUrl: './views/outpost.html',
+                    controller: 'outpostCtrl'
+                })
+                .state('contact',{
+                    url:'/contact',
+                    templateUrl: './views/contact.html',
+                    controller: 'contactCtrl'
+                })
+                .state('join',{
+                    url:'/join',
+                    templateUrl: './views/join.html',
+                    controller: 'joinCtrl'
+                });
+
+            $urlRouterProvider
+                .otherwise('/');
 
 
-    
-  });
+});
