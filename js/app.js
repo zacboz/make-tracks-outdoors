276ed4d0219a319da044c5ac81a35a1055341a8f
zacboz <zacboz@gmail.com>
2017-04-03 16:36:29 -0600

    worked on navbar

--- js/app.js
+++ js/app.js
@@ -2,37 +2,37 @@ angular.module('makeTracks', ['ui.router'])
   .config(function($stateProvider, $urlRouterProvider){
 
     $stateProvider
-                .state('home',{
-                    url:'/',
-                    templateUrl: "./views/home.html",
-                    controller: 'homeCtrl'
-                })
-                .state('story',{
-                    url:'/story',
-                    templateUrl: './views/story.html'
-                })
-                .state('hammocks',{
-                    url:'/hammocks',
-                    templateUrl: './views/hammocks.html',
-                    controller: 'hammocksCtrl'
-                })
-                .state('outpost',{
-                    url:'/outpost',
-                    templateUrl: './views/outpost.html',
-                    controller: 'outpostCtrl'
-                })
-                .state('contact',{
-                    url:'/contact',
-                    templateUrl: './views/contact.html'
-                })
-                .state('join',{
-                    url:'/join',
-                    templateUrl: './views/join.html',
-                    controller: 'joinCtrl'
-                });
+      .state('home',{
+          url:'/',
+          templateUrl: "./views/home.html",
+          controller: 'homeCtrl'
+      })
+      .state('story',{
+          url:'/story',
+          templateUrl: './views/story.html'
+      })
+      .state('hammocks',{
+          url:'/hammocks',
+          templateUrl: './views/hammocks.html',
+          controller: 'hammocksCtrl'
+      })
+      .state('outpost',{
+          url:'/outpost',
+          templateUrl: './views/outpost.html',
+          controller: 'outpostCtrl'
+      })
+      .state('contact',{
+          url:'/contact',
+          templateUrl: './views/contact.html'
+      })
+      .state('join',{
+          url:'/join',
+          templateUrl: './views/join.html',
+          controller: 'joinCtrl'
+      });
 
-            $urlRouterProvider
-                .otherwise('/');
+  $urlRouterProvider
+      .otherwise('/');
 
 
 });
