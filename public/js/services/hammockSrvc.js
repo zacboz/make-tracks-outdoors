5c745750d2e50f7bbe6cb851b52cc60102b53bde
zacboz <zacboz@gmail.com>
2017-05-26 13:09:46 -0600

    updated amazon listing link

--- public/js/services/hammockSrvc.js
+++ public/js/services/hammockSrvc.js
@@ -8,7 +8,7 @@ angular.module('makeTracks').service('hammockSrvc', function(){
         image: "./img/hammocks/product/sunrisehammock.jpg",
         image1: "./img/hammocks/product/sunrisehammockandbag.jpg",
         image2: "",
-        status: 'Coming May 2017',
+        status: 'Live June 1st on Amazon',
         desc: 'The best hammock out there! Great for 1, perfect for 2. Easily transportable and stuffs right into the attached sack. Set up in seconds because our ropes all come with pretied knots!',
         price: 34.99
     }, {
@@ -18,7 +18,7 @@ angular.module('makeTracks').service('hammockSrvc', function(){
         image: "./img/hammocks/product/wavehammock.jpg",
         image1: "./img/hammocks/product/wavehammockandbag.jpg",
         image2: "",
-        status: 'Coming May 2017',
+        status: 'Live June 1st on Amazon',
         desc: 'The best hammock out there! Great for 1, perfect for 2. Easily transportable and stuffs right into the attached sack. Set up in seconds because our ropes all come with pretied knots!',
         price: 34.99
     },{
@@ -28,7 +28,7 @@ angular.module('makeTracks').service('hammockSrvc', function(){
         image: "./img/hammocks/product/oasishammock.jpg",
         image1: "./img/hammocks/product/oasishammockandbag.jpg",
         image2: "",
-        status: 'Coming May 2017',
+        status: 'Live June 1st on Amazon',
         desc: 'The best hammock out there! Great for 1, perfect for 2. Easily transportable and stuffs right into the attached sack. Set up in seconds because our ropes all come with pretied knots!',
         price: 34.99
     }]
@@ -44,9 +44,10 @@ angular.module('makeTracks').service('hammockSrvc', function(){
         image4: "./img/gallery/treeSetUpSq.jpg",
         image5: "./img/gallery/sunrise/AFCoupleSunriseSq.jpg",
         image6: "./img/gallery/sunrise/sunriseDirtBike.jpg",
-        status: 'Coming May 2017',
+        status: 'Live June 1st on Amazon',
         desc: 'The best hammock out there! Great for 1, perfect for 2. Easily transportable and stuffs right into the attached sack. Set up in seconds because our ropes all come with pretied knots!',
-        price: 34.99
+        price: 34.99,
+        link: 'https://www.amazon.com/dp/B06Y1N2691?th=1'
     }, {
         id: 2,
         name: 'The Wave',
@@ -58,9 +59,10 @@ angular.module('makeTracks').service('hammockSrvc', function(){
         image4: "./img/gallery/treeSetUpSq.jpg",
         image5: "./img/gallery/wave/waveAFCanyon2.jpg",
         image6: "./img/gallery/wave/waveDirtBike.jpg",
-        status: 'Coming May 2017',
+        status: 'Live June 1st on Amazon',
         desc: 'The best hammock out there! Great for 1, perfect for 2. Easily transportable and stuffs right into the attached sack. Set up in seconds because our ropes all come with pretied knots!',
-        price: 34.99
+        price: 34.99,
+        link: 'https://www.amazon.com/dp/B06Y1DRKP5?th=1'
     },{
         id: 3,
         name: 'The Woodland',
@@ -72,9 +74,10 @@ angular.module('makeTracks').service('hammockSrvc', function(){
         image4: "./img/gallery/treeSetUpSq.jpg",
         image5: "./img/gallery/woodland/WoodlandSqThumbnail.jpg",
         image6: "./img/gallery/woodland/woodlandUtahValley.jpg",
-        status: 'Coming May 2017',
+        status: 'Live June 1st on Amazon',
         desc: 'The best hammock out there! Great for 1, perfect for 2. Easily transportable and stuffs right into the attached sack. Set up in seconds because our ropes all come with pretied knots!',
-        price: 34.99
+        price: 34.99,
+        link: 'https://www.amazon.com/dp/B06Y1DRNM3?th=1'
     },{
         id: 4,
         name: 'The Summit',
@@ -86,9 +89,10 @@ angular.module('makeTracks').service('hammockSrvc', function(){
         image4: "./img/gallery/treeSetUpSq.jpg",
         image5: "./img/gallery/summit/summitAFCanyon2.jpg",
         image6: "./img/gallery/summit/summitAFCanyon.jpg",
-        status: 'Coming May 2017',
+        status: 'Live June 1st on Amazon',
         desc: 'The best hammock out there! Great for 1, perfect for 2. Easily transportable and stuffs right into the attached sack. Set up in seconds because our ropes all come with pretied knots!',
-        price: 34.99
+        price: 34.99,
+        link: 'https://www.amazon.com/dp/B06Y1T31BQ?th=1'
     },{
         id: 5,
         name: 'The Breeze',
@@ -100,9 +104,10 @@ angular.module('makeTracks').service('hammockSrvc', function(){
         image4: "./img/gallery/treeSetUpSq.jpg",
         image5: "./img/gallery/breeze/AFBreezeSq.jpg",
         image6: "./img/gallery/breeze/breezeAFCanyon.jpg",
-        status: 'Coming May 2017',
+        status: 'Live June 1st on Amazon',
         desc: 'The best hammock out there! Great for 1, perfect for 2. Easily transportable and stuffs right into the attached sack. Set up in seconds because our ropes all come with pretied knots!',
-        price: 34.99
+        price: 34.99,
+        link: 'https://www.amazon.com/dp/B06Y1Y2XG3?th=1'
     }]
 
     this.getBestSellers = () => {
